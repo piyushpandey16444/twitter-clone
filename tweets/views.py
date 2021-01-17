213d61b760cb01bf79f6335f47dfe466e72770f5
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-01-18 00:49:26 +0530

    created templates for base and extends it to home

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
 
-# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return render(request, 'pages/home.html')
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Can be consumed by using any thing including JS, react, JAVA etc..
+    """
+    data = {
+        "id": tweet_id,
+        # "image": tweet_obj.image
+    }
+    status = 200
+    try:
+        tweet_obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = tweet_obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+        # raise Http404
+
+    # json.dumps content_type=application/json
+    return JsonResponse(data, status=status)
+    # return HttpResponse(f"<h1>Hello {tweet_id}-{tweet_obj}</h1")
