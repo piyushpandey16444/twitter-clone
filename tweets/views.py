2770829dbd74ed89c62f87417f31d5a3ef466fb2
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-01-18 19:50:25 +0530

    Safe URL Redirect.

--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 import random
 from .forms import TweetForm
+from django.utils.http import is_safe_url
+from django.conf import settings
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 
 def home_view(request, *args, **kwargs):
@@ -21,7 +25,7 @@ def tweet_create_view(request, *args, **kwargs):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
-        if next_url != None:
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
