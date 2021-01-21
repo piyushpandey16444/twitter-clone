22a3c89dce889c635f1a9b79b0f80d616dfff7ed
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-01-21 16:37:56 +0530

    Adding a Like Field

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Tweet
+from .models import Tweet, TweetLikeUser
+
+
+class TweetLikeUser(admin.TabularInline):
+    model = TweetLikeUser
 
 
 class TweetAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'user']
     search_fields = ['content', 'user__username', 'user__email']
     list_filter = ['user__username', 'user__email']
+    inlines = [TweetLikeUser]
 
     class Meta:
         model = Tweet
--- tweets/models.py
+++ tweets/models.py
@@ -5,11 +5,20 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 
+class TweetLikeUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(
+        User, related_name='tweet_user', blank=True, through=TweetLikeUser)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(
         upload_to="images/", max_length=100, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
