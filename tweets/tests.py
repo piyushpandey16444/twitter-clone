387594469e2c3bad7c794551dbb88320f8d611c8
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-01-23 15:49:03 +0530

    Setting up tests in django 1

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Tweet
+from rest_framework.test import APIClient
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TweetTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="PP", password="admin")
+        tweet_obj = Tweet.objects.create(content="my Tweet", user=self.user)
+
+    def test_tweet_created(self):
+        tweet_obj = Tweet.objects.create(content="my Tweet", user=self.user)
+        self.assertEqual(tweet_obj.id, 2)
+        self.assertEqual(tweet_obj.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password="admin")
+        return client
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/")
+        self.assertEqual(response.status_code, 200)
+        print(response.json())
--- tweets/urls.py
+++ tweets/urls.py
@@ -9,7 +9,7 @@ Base ENDPOINT  /api/tweets/
 
 urlpatterns = [
     path('', tweet_list_view, name='tweets'),
-    path('/action/', tweet_action_view, name="tweet-action-view"),
+    path('action/', tweet_action_view, name="tweet-action-view"),
     path('create/', tweet_create_view, name="create-tweet"),
     path('<int:tweet_id>/', tweet_detail_view, name="tweet-detail-view"),
     path('<int:tweet_id>/delete/', tweet_delete_view, name="tweet-delete-view"),
