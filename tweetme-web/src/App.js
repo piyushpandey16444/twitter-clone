4d9fc481029984187903016d82b364275b2fbe6d
piyushpandey16444 <piyushpandey16444@gmail.com>
2021-01-24 16:07:11 +0530

    React useState and useEffect hooks understanding.

--- tweetme-web/src/App.js
+++ tweetme-web/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [tweets, setTweet] = useState([1, 2, 3, 4, 5])
+
+  useEffect(() => {
+    // do my look up
+    const tweetItems = [{"content": "YouThere"}, {"content": "Hello World"}]
+    setTweet(tweetItems)
+  }, [])
+
   return (
     <div className="App">
-      
+      <p>
+        {tweets.map((tweet, index) => {
+          return <li>{tweet.content}</li>
+        })}
+      </p>
     </div>
   );
 }
